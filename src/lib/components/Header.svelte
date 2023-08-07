fc374c9b62e7acdd5111688841d8e757d1bc6490
meet1803 <20it102@charusat.edu.in>
2023-08-07 12:36:19 +0530

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+    import NavBar from "./NavBar.svelte";
+    import logo from "../images/logo-main.png";
+
+    const NavList = [
+        {name: "Page" , link: "/page"},
+        {name: "Portfolio" , link: "/Portfolio"},
+        {name: "Blog" , link: "/Blog"},
+        {name: "Shop" , link: "/Shop"}
+    ];
+</script>
+
+<header class="bg-gray-900 mt-4 mr-4 max-w-lg">
+
+        <div class="items-center w-4 h-4">
+            <img src={logo} class="w-4 h-4" alt="leroux">
+
+            <div class="items-center">
+                <NavBar name="Home" link="/" />
+
+                {#each NavList as item}
+                    <NavBar name={item.name} link={item.link} />
+                {/each}
+            </div>
+        </div>
+    
+
+</header>
+
---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let name = 'Home';
+	export let link = '/';
+</script>
+
+<a
+	class="  hover:bg-gray-200 py-2 px-4 rounded-md transition-all duration-300 group flex items-center gap-1"
+	href={link}
+>
+	<span class=" font-medium">{name}</span>
+</a>
